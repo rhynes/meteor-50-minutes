7b08f9211962e8daa5ababc2df1a3c8cfac7cc88
rhynes <31453706+rhynes@users.noreply.github.com>
2017-10-07 21:18:40 -1000

    Task 5: subscriptions

--- app/tasklist.js
+++ app/tasklist.js
@@ -1,6 +1,8 @@
 Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
+  Meteor.subscribe('tasks');
+
   Template.tasks.helpers({
     tasks: function() {
       return Tasks.find({}, {sort: {createdAt: -1}});
@@ -10,16 +12,14 @@ if (Meteor.isClient) {
   Template.tasks.events({
     "submit .add-task": function(event) {
       let name = event.target.name.value;
-      Tasks.insert({
-        name: name, createdAt: new Date()
-      });
+      Meteor.call('addTask', name);
       event.target.name.value = '';
       return false;
     },
 
     "click .delete-task": function(event) {
       if (confirm('Delete Task?')) {
-        Tasks.remove(this._id);
+        Meteor.call('deleteTask', this._id);
       }
       return false;
     }
@@ -27,5 +27,22 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-
+  Meteor.publish('tasks', function() {
+    return Tasks.find({userId: this.userId});
+  })
 }
+
+Meteor.methods({
+  addTask: function(name){
+    if (!Meteor.userId()) {
+      throw new Meteor.Error('No Access');
+    }
+    Tasks.insert({
+      name: name, createdAt: new Date(), userId: Meteor.userId()
+    });
+  },
+
+  deleteTask: function(taskId) {
+    Tasks.remove(taskId);
+  }
+})
